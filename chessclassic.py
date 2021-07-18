0755f5348193508548e5991bea8c3f0b5e3775c4
Juho <juho-kim@outlook.com>
2021-07-17 20:37:07 -0400

    Fixed code

--- chessclassic.py
+++ chessclassic.py
@@ -13,14 +13,13 @@ class GameFrame(BaseGameFrame):
 
 class ChessClassicApp(wx.App):
     def __init__(self, *args, **kwargs):
+        self.menu_frame = None
+        self.game_frames = []
+
         super().__init__(*args, **kwargs)
 
-        self.menu_frame = None
-        self.game_frame = None
-
     def OnInit(self):
-        self.menu_frame = MenuFrame(None, wx.ID_ANY, 'Menu')
-        self.game_frame = GameFrame(None, wx.ID_ANY, 'Game')
+        self.menu_frame = MenuFrame(None, wx.ID_ANY, 'ChessClassic Menu')
 
         self.SetTopWindow(self.menu_frame)
         self.menu_frame.Show()
